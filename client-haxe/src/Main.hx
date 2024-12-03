e07a1f0fbb661d45e608508f3411c2e9b8305f5b
Jannick Oursin <github@joursin.com>
2024-12-03 13:52:34 +0100

    set up openfl project

--- client-haxe/src/Main.hx
+++ client-haxe/src/Main.hx
@@ -1,11 +1,18 @@
 package;
 
+import openfl.text.TextFormat;
+import openfl.text.TextField;
 import openfl.display.Sprite;
 
-class Main extends Sprite
-{
-	public function new()
-	{
+class Main extends Sprite {
+	public function new() {
 		super();
+
+		var textField = new TextField();
+		textField.setTextFormat(new TextFormat(null, 20));
+		textField.text = "Where Game? :(";
+		textField.x = 400;
+		textField.y = 300;
+		addChild(textField);
 	}
 }
